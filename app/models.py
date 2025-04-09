68077dbfa9ecb0f9afd6a28083750e9921c007c9
CasstielP <cpss5433@gmail.com>
2025-04-09 10:07:02 -0700

    add id, username, password to user model

--- app/models.py
+++ app/models.py
@@ -4,7 +4,14 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(100),unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.string(128), unique=True, nullable=False)
 
     def to_dict(self):
-        return {"id": self.id, "name": self.name}
+        return {
+            "id": self.id, 
+            "username": self.username,
+            "email": self.email
+                
+                }
