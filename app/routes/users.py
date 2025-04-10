d98fd78f96f25649ca8fe77f095cdd201785eb4d
CasstielP <cpss5433@gmail.com>
2025-04-10 10:31:24 -0700

    add csrf token, jwt token feature

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,7 +1,9 @@
 from flask import request, jsonify
 from app.routes import api
 from app.models import db, User
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import (create_access_token, 
+jwt_required, get_jwt_identity, create_refresh_token, set_access_cookies, 
+set_refresh_cookies, unset_jwt_cookies, get_csrf_token)
 
 
 @api.route('/users', methods=['GET'])
@@ -46,12 +48,20 @@ def login():
     if not user or not user.check_password(password):
         return jsonify({"error": "Invalid credentials"}), 401
     
-    access_token = create_access_token(identity=user.id)
+    access_token = create_access_token(identity=str(user.id))
+    refresh_token = create_refresh_token(identity=str(user.id))
+
     
-    return jsonify({"message": "Login successful",
+    response = jsonify({"message": "Login successful",
                     "access_token": access_token,
+                    "csrf_access_token": get_csrf_token(access_token),
+                    "csrf_refresh_token": get_csrf_token(refresh_token),
                     "user": user.to_dict()
-                    }), 200 
+                    })
+    set_access_cookies(response, access_token)
+    set_refresh_cookies(response, refresh_token)
+
+    return response
     
 
 
@@ -61,3 +71,25 @@ def get_my_profile():
     user_id = get_jwt_identity()
     user = User.query.get(user_id)
     return jsonify(user.to_dict()), 200
+
+
+@api.route('/refresh', methods=["POST"])
+@jwt_required(refresh=True)
+def refresh():
+    user_id = get_jwt_identity()
+    new_access_token = create_access_token(identity=user_id)
+
+    response = jsonify({
+        "access_token": new_access_token
+    })
+
+    return response
+
+
+
+#logout
+@api.route('/logout', methods=["POST"])
+def logout():
+    response = jsonify({"message": "You have logged out"})
+    unset_jwt_cookies(response)
+    return response
