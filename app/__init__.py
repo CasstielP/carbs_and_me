e953d65d28d75b9e57ca3bdfe8897adb0f3c3e49
CasstielP <cpss5433@gmail.com>
2025-04-09 17:45:49 -0700

    add jwt function

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
+from flask_jwt_extended import JWTManager
+
+jwt = JWTManager()
+
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -12,6 +16,7 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
+    jwt.init_app(app)
 
     from app.routes import api as api_blueprint
     app.register_blueprint(api_blueprint, url_prefix='/api')
--- app/routes/users.py
+++ app/routes/users.py
@@ -1,6 +1,8 @@
 from flask import request, jsonify
 from app.routes import api
 from app.models import db, User
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+
 
 @api.route('/users', methods=['GET'])
 def get_users():
@@ -15,6 +17,12 @@ def create_user():
     password = data.get("password")
     if not username or not email or not password:
         return jsonify({"error": "Username, email, and password are required"}), 400
+    
+    if User.query.filter_by(email=email).first():
+        return jsonify({"error": "Email already exists"}), 409
+    if User.query.filter_by(username=username).first():
+        return jsonify({"error": "Username already exists"}), 409
+
 
     user = User(username=username, email=email)
     user.set_password(password)
@@ -38,7 +46,18 @@ def login():
     if not user or not user.check_password(password):
         return jsonify({"error": "Invalid credentials"}), 401
     
+    access_token = create_access_token(identity=user.id)
+    
     return jsonify({"message": "Login successful",
+                    "access_token": access_token,
                     "user": user.to_dict()
                     }), 200 
     
+
+
+@api.route('/me', methods=['GET'])
+@jwt_required()
+def get_my_profile():
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+    return jsonify(user.to_dict()), 200
